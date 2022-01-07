ad453940bc0bfbf21f472c2f6200d3c0e53dcc5b
Clive <cplan082@uottawa.ca>
2022-01-07 12:24:29 -0500

    Succesfully interfaced Buttons of Xbox controller with python script

--- xbox_controller_interface/xbox_controller_interface.py
+++ xbox_controller_interface/xbox_controller_interface.py
@@ -1,13 +1,87 @@
-#import evdev
 from evdev import InputDevice, categorize, ecodes
 
-#creates object 'gamepad' to store the dataa
-#you can call it whatever you like
-gamepad = InputDevice('/dev/input/event2')
+gamepad = InputDevice('/dev/input/event7')
 
-#prints out device info at start
-print(gamepad)
+# button code variables
+btn_a = 304
+btn_b = 305
+btn_x = 307
+btn_y = 308
 
-#evdev takes care of polling the controller in a loop
+btn_leftBumper = 310
+btn_rightBumper = 311
+
+btn_leftJoy = 317
+btn_rightJoy = 318
+
+btn_start = 315
+btn_back = 158
+btn_home = 316 # Backlight Xbox home button
+ 
+# analog inputs code variables
+## Joystick max val = 65535 (2 bytes)
+al_leftJoy_x = 0
+al_leftJoy_y = 1  # Axis is inverted
+al_rightJoy_x = 2
+al_rightJoy_y = 5  # Axis is inverted
+
+## Trig max val = 1023 (10 bits)
+al_rightTrig = 9
+al_leftTrig = 10
+
+## D-pad val can be either 1, 0, or -1
+al_dpad_x = 16
+al_dpad_y = 17  # Axis is inverted
+
+
+#loop and filter by event code
 for event in gamepad.read_loop():
-    print(categorize(event))
+    
+    if event.type == ecodes.EV_KEY:
+        
+        if event.value == 1:
+            
+            if event.code == btn_a:
+                continue
+#                 print("A")
+                
+            elif event.code == btn_b:
+                continue
+#                 print("B")
+                
+            elif event.code == btn_x:
+                continue
+#                 print("X")
+                
+            elif event.code == btn_y:
+                continue
+#                 print("Y")
+                
+            elif event.code == btn_leftBumper:
+                continue
+#                 print("Left Bumper")
+                
+            elif event.code == btn_rightBumper:
+                continue
+#                 print("Right Bumper")
+                
+            elif event.code == btn_start:
+                continue
+#                 print("Start")
+                
+            elif event.code == btn_home:
+                continue
+#                 print("Home")
+                
+            elif event.code == btn_back:
+                continue
+#                 print("Back")
+                
+            elif event.code == btn_leftJoy:
+                continue
+#                 print("Left Joystick")
+            
+            elif event.code == btn_rightJoy:
+                continue
+#                 print("Right Joystick")
+ 
